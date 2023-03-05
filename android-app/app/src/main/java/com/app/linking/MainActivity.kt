0b2a863045b2bbec6fb5cae763253ebfc03531a7
patildnyaneshwar <dnyaneshwar1926@gmail.com>
2023-03-05 13:53:34 +0530

    initial commit

--- android-app/app/src/main/java/com/app/linking/MainActivity.kt
+++ android-app/app/src/main/java/com/app/linking/MainActivity.kt
@@ -2,10 +2,16 @@ package com.app.linking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent: Intent = intent
+        val appLinkAction: String? = appLinkIntent.action
+        val appLinkData: Uri? = appLinkIntent.data
     }
 }
