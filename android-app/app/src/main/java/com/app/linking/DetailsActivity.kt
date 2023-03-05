0b2e625fb3160e3de954d50bfcdceb2116f43f66
patildnyaneshwar <dnyaneshwar1926@gmail.com>
2023-03-05 14:30:46 +0530

    handle app link data

--- android-app/app/src/main/java/com/app/linking/DetailsActivity.kt
+++ android-app/app/src/main/java/com/app/linking/DetailsActivity.kt
@@ -1,11 +1,29 @@
 package com.app.linking
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class DetailsActivity : AppCompatActivity() {
+    private val TAG = "DetailsActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        supportActionBar?.title = "Details Activity"
+        val appLinkIntent: Intent = intent
+        val appLinkAction: String? = appLinkIntent.action
+        val appLinkData: Uri? = appLinkIntent.data
+
+        val textView = findViewById<TextView>(R.id.text_view)
+
+        Log.d(TAG, "onCreate: appLinkAction: $appLinkAction \nappLinkData: $appLinkData")
+        if (Intent.ACTION_VIEW == appLinkAction) {
+            textView.text = appLinkData?.getQueryParameter("inputData");
+            Log.d(TAG, "onCreate: Query: ${appLinkData?.query}")
+        }
     }
 }
--- android-app/app/src/main/java/com/app/linking/MainActivity.kt
+++ android-app/app/src/main/java/com/app/linking/MainActivity.kt
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Main Activity"
+
         // ATTENTION: This was auto-generated to handle app links.
         val appLinkIntent: Intent = intent
         val appLinkAction: String? = appLinkIntent.action
         val appLinkData: Uri? = appLinkIntent.data
+
+        val textView = findViewById<TextView>(R.id.text_view)
+
+        Log.d(TAG, "onCreate: appLinkAction: $appLinkAction \nappLinkData: $appLinkData")
+        if (Intent.ACTION_VIEW == appLinkAction) {
+            textView.text = appLinkData?.getQueryParameter("inputData");
+            Log.d(TAG, "onCreate: Query: ${appLinkData?.query}")
+        }
+
     }
 }
